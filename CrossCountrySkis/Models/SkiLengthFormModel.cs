1cb625f8b5f45817ef21458f7934474c904df136
oskii96 <trummsocken@hotmail.com>
2024-09-17 22:08:14 +0200

    Refactored some code

--- CrossCountrySkis/Models/SkiLengthFormModel.cs
+++ CrossCountrySkis/Models/SkiLengthFormModel.cs
@@ -5,6 +5,8 @@ namespace CrossCountrySkis.Models
 {
     public class SkiLengthFormModel
     {
+        [Required]
+        [Range(0, 140, ErrorMessage = "Please enter a valid age.")]
         public int Age { get; set; }
         [Required]
         [Range(1, 300, ErrorMessage = "Please enter a valid length.")]
--- CrossCountrySkis/Models/SuggestedSkiLengthResult.cs
+++ CrossCountrySkis/Models/SuggestedSkiLengthResult.cs
@@ -2,7 +2,23 @@
 {
     public class SuggestedSkiLengthResult
     {
+        private SkiLengthSpan? _skiLengthSpan;
         public int SkiLength { get; set; }
-        public SkiLengthSpan? SkiLengthSpan { get; set; }
+        public SkiLengthSpan? SkiLengthSpan
+        {
+            get => _skiLengthSpan;
+            set
+            {
+                if (value is not null && value.LowerSpan == value.UpperSpan)
+                {
+                    SkiLength = value.LowerSpan;
+                    _skiLengthSpan = null;
+                }
+                else
+                {
+                    _skiLengthSpan = value;
+                }
+            }
+        }
     }
 }
